393cee163a6d67b4ef8e2907dc0041ad648ea1eb
webdev-akshay <webdev.akshay@gmail.com>
2025-02-12 13:18:56 +0530

    ui changes

--- src/app/components/jobboard/jobboard.component.ts
+++ src/app/components/jobboard/jobboard.component.ts
@@ -12,12 +12,18 @@ import { FormsModule } from '@angular/forms';
 })
 export class JobboardComponent implements OnInit{
   jobData:any[]=[];
+  filteredJobs:any[]=[];
   searchedQuery:string=''
   constructor(private jobService:JobboardService){}
   getJob(){
     this.jobService.getJobDetails(this.searchedQuery).subscribe((data)=>{
-      this.jobData=data
+      this.jobData=data.data
       console.log(data);
+    })
+  }
+  filterJobs(jobType:string){
+    this.jobService.getJobDetails(jobType).subscribe((data)=>{
+      this.filteredJobs=data.data.fil  
     })
   }
   ngOnInit(): void {
